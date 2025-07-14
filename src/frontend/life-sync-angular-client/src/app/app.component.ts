97a40006713e5dd0e9938ec41888082b001daa52
Iliya Nankov <nankoviliya@gmail.com>
2025-07-14 21:36:51 +0300

    fe-angular: header and footer components added

--- src/frontend/life-sync-angular-client/src/app/app.component.ts
+++ src/frontend/life-sync-angular-client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { FooterComponent } from 'components/footer/footer.component';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, ButtonModule],
+  imports: [RouterOutlet, RouterModule, ButtonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
