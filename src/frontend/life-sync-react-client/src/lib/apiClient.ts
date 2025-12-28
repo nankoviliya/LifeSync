2a6722bfd45c365f40dd9fb688b156a472fef8a7
Iliya Nankov <nankoviliya@gmail.com>
2025-12-29 01:18:11 +0200

    SessionExpiredNotification component removed

--- src/frontend/life-sync-react-client/src/lib/apiClient.ts
+++ src/frontend/life-sync-react-client/src/lib/apiClient.ts
@@ -37,7 +37,7 @@ apiClient.interceptors.response.use(
     if (!refreshPromise) {
       refreshPromise = refreshToken()
         .catch((err) => {
-          window.location.href = '/login?session_expired=true';
+          window.location.href = '/login';
           throw err;
         })
         .finally(() => {
