539a3c195e21974f3dfc0dc655853a77db7ad0a8
Iliya Nankov <nankoviliya@gmail.com>
2026-03-22 13:18:43 +0200

    Added UserProfile loading skeletone

--- src/frontend/life-sync-react-client/src/features/userProfile/components/UserProfile.tsx
+++ src/frontend/life-sync-react-client/src/features/userProfile/components/UserProfile.tsx
@@ -1,16 +1,23 @@
+import { Skeleton } from '@/components/ui/skeleton';
 import { ExportAccountData } from '@/features/userProfile/components/dataExport/ExportAccountData';
 import { ImportAccountData } from '@/features/userProfile/components/dataImport/ImportAccountData';
 import { UserProfileDataContainer } from '@/features/userProfile/components/profileData/UserProfileDataContainer';
 import { useAuth } from '@/stores/AuthProvider';
 
+export const UserProfileDataContainerSkeleton = () => (
+  <div className="flex flex-col gap-2">
+    <Skeleton className="h-12 w-12 rounded-full" />
+    <Skeleton className="h-4 w-[200px]" />
+  </div>
+);
+
 export const UserProfile = () => {
-  const { isLoading, isAuthenticated, user } = useAuth();
+  const { isLoading, user } = useAuth();
 
   return (
     <div className="p-4">
-      {isLoading && <p>Loading...</p>}
-      {!isLoading && !user && <p>Unable to find user profile data</p>}
-      {isAuthenticated && user && (
+      {isLoading && <UserProfileDataContainerSkeleton />}
+      {!isLoading && user && (
         <div className="flex flex-col items-center gap-6 md:flex-row md:items-start">
           <UserProfileDataContainer userData={user} />
           <div className="flex flex-col gap-6">
